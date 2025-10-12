f2aed4c913af8fa6d75ab501b1d2d65530872f45
harsh <nh1750501@gmail.com>
2025-10-12 17:34:14 +0530

    Added conditions

--- frontend/src/components/ProfileModal.jsx
+++ frontend/src/components/ProfileModal.jsx
@@ -9,9 +9,18 @@ const ProfileModal = ({ isOpen, onClose }) => {
   const {user,setUser}=useChatState()
   // state for toggle view and edit images
 
-  const[isEditing,setisEditing]=useState(false)
+  const[IsEditing,setIsEditing]=useState(false)
   const [name,setName]=useState(user?.user?.name || "")
   const [bio,setBio]=useState(user?.user?.bio || "user is mysterious")
   const[profilePic,setProfilePic]=useState(user?.user?.pic)
+  useEffect(()=>{
+    if(user?.user){
+      setName(user.user.name)
+      setBio(user.user.bio || "user is mysterious")
+      setProfilePic(user.user.pic);
+    }
+    if(!isOpen) setIsEditing(false)
+      // this runs only when user opens it and details are changed
+  },[isOpen,user])
 
 }
