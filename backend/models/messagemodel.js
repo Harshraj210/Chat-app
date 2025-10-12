a3152b284f9cd6ccc89d9d7bd601514f6b2b7a4d
harsh <nh1750501@gmail.com>
2025-10-12 19:06:06 +0530

    updated  message models for image transfer

--- backend/models/messagemodel.js
+++ backend/models/messagemodel.js
@@ -1,25 +1,30 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    require: true,
+const messageSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      require: true,
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      // ID comes from user collection
+      ref: "User",
+    },
+    readBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    mediaUrl: { type: String }, // To store the URL of the photo or video
+    mediaType: { type: String }, // To store the type (e.g., 'image' or 'video')
   },
-  chat: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Chat",
-  },
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    // ID comes from user collection
-    ref: "User",
-  },
-  readBy:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
+  {
+    timestamps: true,
   }
-},{
-  timestamps:true
-});
-const Message = new mongoose.model("Message",messageSchema)
-export default Message
+);
+const Message = new mongoose.model("Message", messageSchema);
+export default Message;
