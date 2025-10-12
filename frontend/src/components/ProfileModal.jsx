a6138c83a39d3a668975439c7838102486dc7f95
harsh <nh1750501@gmail.com>
2025-10-12 17:28:18 +0530

    UPdated code base

--- frontend/src/components/ProfileModal.jsx
+++ frontend/src/components/ProfileModal.jsx
@@ -3,3 +3,15 @@ import axios from "axios";
 import { useChatState } from "../context/ChatProvider";
 import { Motion, AnimatePresence } from "framer-motion";
 import { toast } from "react-hot-toast";
+
+
+const ProfileModal = ({ isOpen, onClose }) => {
+  const {user,setUser}=useChatState()
+  // state for toggle view and edit images
+
+  const[isEditing,setisEditing]=useState(false)
+  const [name,setName]=useState(user?.user?.name || "")
+  const [bio,setBio]=useState(user?.user?.bio || "user is mysterious")
+  const[profilePic,setProfilePic]=useState(user?.user?.pic)
+
+}
