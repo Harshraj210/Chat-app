1792f57d02f6f573197f2f99a271068e9cfe5de1
harsh <nh1750501@gmail.com>
2025-10-13 10:51:34 +0530

    Added function for file upload in message controller

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
 import protect from "../middleware/authentication.js"
-import { allMessages, sendMessage } from "../controllers/messagecontroller.js";
+import { allMessages, sendMediaMessage, sendMessage } from "../controllers/messagecontroller.js";
 
 const router = express.Router()
 router.route("/:chatId").get(protect,allMessages)
 router.route("/").post(protect,sendMessage)
+router.route("/media").post(protect,sendMediaMessage)
 export default router
